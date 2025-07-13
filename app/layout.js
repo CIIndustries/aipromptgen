ac11b8169c5714731f0e0b018e70999c40bf1bba
CIIndustries <cyberintelindustries@gmail.com>
2025-07-13 13:46:26 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -36,6 +36,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Ezoic Privacy Scripts - MUST BE FIRST */}
+        <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false"></script>
+        <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false"></script>
+        
+        {/* Ezoic Header Script */}
+        <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.ezstandalone = window.ezstandalone || {};
+              ezstandalone.cmd = ezstandalone.cmd || [];
+            `,
+          }}
+        />
+
         <meta name="theme-color" content="#2563eb" />
         <meta name="google-adsense-account" content="ca-pub-4591516845324922" />
         <meta name="pushsdk" content="3181d05f834655701c5b819fbe4cc29e" />
